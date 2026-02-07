cd37ff47bfbb2ba5d91534df21492e6923732869
KTG5672 <zzang5992@gmail.com>
2026-02-07 13:29:22 +0900

    refactor: 헤더에 사용되는 상수 변경, 주석 제거

--- src/main/java/io/github/ktg/temm/app/security/JwtAuthenticationFilter.java
+++ src/main/java/io/github/ktg/temm/app/security/JwtAuthenticationFilter.java
@@ -11,6 +11,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.filter.OncePerRequestFilter;
 import tools.jackson.databind.ObjectMapper;
 
@@ -18,7 +20,6 @@ import tools.jackson.databind.ObjectMapper;
 @Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER_PREFIX = "Bearer ";
 
     private final TokenProvider tokenProvider;
@@ -28,28 +29,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, @Nonnull HttpServletResponse response,
         @Nonnull FilterChain filterChain) throws ServletException, IOException {
 
-        // 1. Header 에서 Authentication 추출
-        String authHeader = request.getHeader(AUTHORIZATION);
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (authHeader == null || !authHeader.startsWith(BEARER_PREFIX)) {
             filterChain.doFilter(request, response);
             return;
         }
-        // 2. "Bearer " Prefix 제거
+
         String token = authHeader.substring(BEARER_PREFIX.length());
         if (token.isEmpty()) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        // 3. 토큰 검증
         try {
             if (tokenProvider.validateAccessToken(token)) {
-                // 4. 토큰에서 유저 식별자 조회
                 String userId = tokenProvider.getUserIdByAccessToken(token);
-
-                // 5. Login Context 등록
                 LoginContext.set(userId);
-
             }
         } catch (ExpiredJwtException e) {
             writeExpiredTokenResponse(response);
@@ -59,7 +54,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private void writeExpiredTokenResponse(HttpServletResponse response) throws IOException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.getWriter().write(
             objectMapper.writeValueAsString(
                 new ErrorResponse(
