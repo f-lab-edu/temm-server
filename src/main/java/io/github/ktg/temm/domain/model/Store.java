b2278ac7951d04271fa0109c4471d23da67b3eb4
KTG5672 <zzang5992@gmail.com>
2026-02-23 23:13:41 +0900

    feat: 스토어, 유저-스토어 도메인 정의 - 도메인 엔티티 정의 - Repository 정의

---
+++ src/main/java/io/github/ktg/temm/domain/model/Store.java
@@ -0,0 +1,25 @@
+package io.github.ktg.temm.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "stores")
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+}
